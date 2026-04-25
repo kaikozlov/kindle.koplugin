da2cfad9944fb8525a3130c3b7a60c614fee4fdc
Kai Kozlov <37962720+kaikozlov@users.noreply.github.com>
2026-04-24 21:06:49 -0500

    fix meta version

--- _meta.lua
+++ _meta.lua
@@ -4,5 +4,5 @@ return {
     name = "kindle",
     fullname = _("Kindle Virtual Library"),
     description = _("Browse Kindle-native books from a virtual library and convert simple DRM-free KFX files to EPUB."),
-    version = "0.1.0",
+    version = "0.0.2",
 }
